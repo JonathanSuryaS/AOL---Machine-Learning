dd7c88f68516e62a379eba456f6d96c39e4df7cb
Jonathan Surya Sanjaya <114159473+JonathanSuryaS@users.noreply.github.com>
2024-05-13 00:58:33 +0700

    Update main.py

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -1,14 +1,2 @@
 import streamlit as st
 from hydralit_components import hydralit_navbar
-
-app = hydralit_navbar(
-    header="My Streamlit App",  # Customize the header text
-    links=[
-        ("Home", "#"),  # Link text and corresponding URL (or anchor for internal navigation)
-        ("About", "#about"),
-        ("Contact", "#contact"),
-        # Add more links as needed
-    ],
-)
-
-st.write(app)
