21e1fa9f94e5659a4da8a5d813bb96c1dd27ade0
Jonathan Surya Sanjaya <114159473+JonathanSuryaS@users.noreply.github.com>
2024-05-13 01:09:12 +0700

    Update main.py

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -1,2 +1,31 @@
 import streamlit as st
-from hydralit_components import hydralit_navbar
+from streamlit_navigation_bar import st_navbar
+
+st.set_page_config(initial_sidebar_state="collapsed")
+
+pages = ["Home", "Library", "Tutorials", "Development", "Download"]
+styles = {
+    "nav": {
+        "background-color": "#7BD192",
+    },
+    "div": {
+        "max-width": "32rem",
+    },
+    "span": {
+        "border-radius": "0.5rem",
+        "padding": "0.4375rem 0.625rem",
+        "margin": "0 0.125rem",
+    },
+    "active": {
+        "background-color": "rgba(255, 255, 255, 0.25)",
+    },
+    "hover": {
+        "background-color": "rgba(255, 255, 255, 0.35)",
+    },
+}
+
+page = st_navbar(pages, styles=styles)
+st.write(page)
+
+with st.sidebar:
+    st.write("Sidebar")
